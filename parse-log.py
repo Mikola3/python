45060a1754e2c629bc69f40aa8b5d9b966c5e087
Mikola3 <34712864+Mikola3@users.noreply.github.com>
2018-12-14 16:03:18 +0300

    Create parse-log.py

---
+++ parse-log.py
@@ -0,0 +1,28 @@
+'''
+google - 1 - http
+onliner - 2 - https
+google - 3 - http
+google - 4 - http
+onliner - 5 - https
+google - 6 - http
+google - 7 - http
+'''
+
+a = 0
+b = 0
+
+f = open('sites.log', "r")
+for line in f:
+    if line.split(' - ')[0] == "google":
+        a +=1
+    elif line.split(' - ')[0] == "onliner":
+        b +=1
+f.close
+
+print("Google:", a)
+print("Onliner:", b)
+
+'''
+Google: 5
+Onliner: 2
+'''

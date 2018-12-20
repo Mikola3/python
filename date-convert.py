b0c90f8f1518026d161d14840f3c65f3fed47c7d
Mikola3 <34712864+Mikola3@users.noreply.github.com>
2018-12-20 13:28:11 +0300

    Update date-convert.py

--- date-convert.py
+++ date-convert.py
@@ -1,12 +1,14 @@
 '''
 dates.log:
-
+1st May 1992
+2nd Dec 1994
 11th Sep 2001
 5th Oct 2005
 3th Apr 2003
 '''
 
 import datetime
+import re
 
 def month_converter(month):
     months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
@@ -16,11 +18,12 @@ file1 = open('dates.log', "r")
 file2 = open('dates2.log', "a+")
 
 for line in file1:
-    number = line.split(' ')[0].split('th')[0]
-    month = line.split(' ')[1]
-    month_num = month_converter(month)
-    year = line.split(' ')[-1]
-    data = datetime.datetime(int(year), int(month_num), int(number))
+    number, month, year = line.split(' ')
+    #print(number, month, year)
+    number_new = re.findall('\d+', number)[0]
+    #print(number_new, month, year)
+    month_new = month_converter(month)
+    data = datetime.datetime(int(year), int(month_new), int(number_new))
     infa = "{:0>4}-{:0>2}-{:0>2}".format(data.year, data.month, data.day)
     print(infa)
     file2.write(infa + '\n')
@@ -29,7 +32,8 @@ file2.close
 
 '''
 dates2.log:
-
+1992-05-01
+1994-12-02
 2001-09-11
 2005-10-05
 2003-04-03
